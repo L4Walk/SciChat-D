f833d4c1979bbf26c5be466d9fb040ee19e72e96
LJY Lu <1005058349@qq.com>
2023-06-08 16:08:15 +0000

    修改了axios

--- app/components/setAPI.tsx
+++ app/components/setAPI.tsx
@@ -1,3 +1,7 @@
+<meta
+  http-equiv="Content-Security-Policy"
+  content="upgrade-insecure-requests"
+/>;
 import Locale from "../locales";
 
 import styles from "./setAPI.module.scss";
@@ -10,6 +14,7 @@ import { useEffect, useState } from "react";
 import { useAccessStore } from "../store";
 import comUtil from "../../common/comUtil";
 import axios from "axios";
+import { Console } from "console";
 
 export function SetAPIModal(props: { onClose: () => void }) {
   return (
@@ -33,12 +38,50 @@ export function MessageSetAPI() {
   const [title, setTitle] = useState("");
   const [body, setBody] = useState("");
 
-  const ApplyKey = (e: { preventDefault: () => void }) => {
+  const ApplyKey = () => {
     if (EmailAddress == "") {
       return;
     }
 
-    e.preventDefault();
+    console.log("申请邮件");
+    axios({
+      method: "POST", // 若不设置，默认为GET请求
+      url: "/chat/pub_chat/createAccountByEmail",
+      //当`url`是相对地址的时候，设置`baseURL`自动添加在url之前会非常方便
+      baseURL: "http://47.113.149.222", //http://47.113.149.222:8080
+      //`headers`选项是需要被发送的自定义请求头信息
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      //`params`选项是要随请求一起发送的请求参数----一般链接在URL后面
+      //他的类型必须是一个纯对象或者是URLSearchParams对象
+      params: {
+        ID: 12345,
+      },
+      //`data`选项是作为一个请求体而需要被发送的数据
+      //该选项只适用于方法：`put/post/patch`
+      data: {
+        email: EmailAddress,
+      },
+      //`timeout`选项定义了请求发出的延迟毫秒数
+      //如果请求花费的时间超过延迟的时间，那么请求会被终止
+      timeout: 1000,
+      //`withCredentails`选项表明了是否是跨域请求,默认是false
+      withCredentials: true, //default,
+      //返回数据的格式
+      //其可选项是arraybuffer,blob,document,json,text,stream
+      responseType: "json", //default
+    })
+      .then((res) => {
+        setPosts((posts) => res.data);
+      })
+      .catch((err) => {
+        console.log("Bug啦");
+        console.log(posts);
+        console.log(EmailAddress);
+        console.log(err.message);
+      });
+
+    //e.preventDefault();
+    /*
     axios
       .post(comUtil.getHost + "/chat/pub_chat/createAccountByEmail", {
         title: title,
@@ -54,10 +97,12 @@ export function MessageSetAPI() {
         setBody("");
       })
       .catch((err) => {
+        console.log("Bug啦");
         console.log(posts);
         console.log(EmailAddress);
         console.log(err.message);
       });
+      */
   };
 
   return (
@@ -115,8 +160,8 @@ export function MessageSetAPI() {
               icon={<AddIcon />}
               text={"获取Key"}
               onClick={() => {
+                ApplyKey();
                 console.log(EmailAddress);
-                ApplyKey;
               }}
               shadow
             />
