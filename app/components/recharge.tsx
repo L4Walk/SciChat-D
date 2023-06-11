364dd9c427a926288b68c9a488504648b4190131
LJY Lu <1005058349@qq.com>
2023-06-11 17:05:03 +0000

    充值界面设置

--- app/components/recharge.tsx
+++ app/components/recharge.tsx
@@ -58,9 +58,54 @@ function useSteps(
   };
 }
 
-{
-  /*
-export function Recharge(){
+function Steps<
+  T extends {
+    name: string;
+    value: string;
+  }[],
+>(props: { steps: T; onStepChange?: (index: number) => void; index: number }) {
+  const steps = props.steps;
+  const stepCount = steps.length;
+
+  return (
+    <div className={styles["steps"]}>
+      <div className={styles["steps-progress"]}>
+        <div
+          className={styles["steps-progress-inner"]}
+          style={{
+            width: `${((props.index + 1) / stepCount) * 100}%`,
+          }}
+        ></div>
+      </div>
+      <div className={styles["steps-inner"]}>
+        {steps.map((step, i) => {
+          return (
+            <div
+              key={i}
+              className={`${styles["step"]} ${
+                styles[i <= props.index ? "step-finished" : ""]
+              } ${i === props.index && styles["step-current"]} clickable`}
+              onClick={() => {
+                props.onStepChange?.(i);
+              }}
+              role="button"
+            >
+              <span className={styles["step-index"]}>{i + 1}</span>
+              <span className={styles["step-name"]}>{step.name}</span>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+}
+
+export function Recharge() {
+  const [price, setPrice] = useState("");
+  const [payType, setPayType] = useState("");
+  const [orderID, setOrderID] = useState("");
+  const [payUrl, setPayUrl] = useState("");
+
   const steps = [
     {
       name: "设置金额",
@@ -73,22 +118,295 @@ export function Recharge(){
   ];
 
   const { currentStep, setCurrentStepIndex, currentStepIndex } =
-  useSteps(steps);
+    useSteps(steps);
 
-  return(
+  return (
     <>
-      <div className={"充值额度"} style={currentStep.value !== "setPrice" ? { display: "none" } : {}}>
-
-      
-      </div>
-
+      <div
+        className={"充值额度"}
+        style={currentStep.value !== "setPrice" ? { display: "none" } : {}}
+      ></div>
     </>
-  )
+  );
 }
-*/
+
+export function SetRecharge1(props: { onClose: () => void }) {
+  const accessStore = useAccessStore();
+
+  const [price, setPrice] = useState("");
+  const [payType, setPayType] = useState("");
+  const [orderID, setOrderID] = useState("");
+  const [payUrl, setPayUrl] = useState("");
+
+  const [payQrUrl, setPayQrUrl] = useState("");
+  const [isShowPayQr, setShowPayQr] = useState("");
+  const [posts, setPosts] = useState([]);
+  const [title, setTitle] = useState("");
+  const [body, setBody] = useState("");
+
+  const steps = [
+    {
+      name: "设置金额",
+      value: "setPrice",
+    },
+    {
+      name: "获取二维码",
+      value: "setQr",
+    },
+  ];
+
+  const { currentStep, setCurrentStepIndex, currentStepIndex } =
+    useSteps(steps);
+
+  function showPayQr(props: {}) {
+    if (price.length == 0 || Number(price) <= 0) {
+      return;
+    }
+
+    setOrderID(comUtil.getGuid());
+    createPayQrCode();
+    setShowPayQr("true");
+  }
+
+  function createPayQrCode() {
+    axios
+      .post(comUtil.getHost() + "/chat/pub_chat/getPayQrCode", {
+        Headers: { "Content-Type": "application/x-www-form-urlencoded" },
+        data: {
+          pay_type: payType,
+          price: price,
+          order_id: orderID,
+          order_uid: accessStore.token,
+        },
+      })
+      .then((res) => {
+        setPosts((posts) => res.data);
+        if (res.data.status == "ok") {
+          setPayUrl(res.data.info.qr);
+          setPayQrUrl("https://xorpay.com/qr?data=" + payUrl);
+        }
+      })
+      .catch((err) => {
+        console.log("Bug啦");
+        console.log(err.message);
+      });
+  }
+
+  function payOver() {
+    loadPaccount();
+    setPrice("");
+    setPayType("");
+    setOrderID("");
+    window.close();
+  }
+
+  function loadPaccount() {
+    axios;
+  }
+
+  function ShowQr() {
+    return (
+      <>
+        <div className={"recharge"}>
+          <List>
+            <ListItem title="充值金额">
+              <Input
+                type="text"
+                value={"充值金额：" + price}
+                readOnly={true}
+              ></Input>
+            </ListItem>
+
+            <ListItem title="订单号">
+              <Input
+                type="text"
+                value={"订单号：" + orderID}
+                readOnly={true}
+              ></Input>
+            </ListItem>
+
+            <ListItem title="二维码">
+              <button title="关闭"></button>
+            </ListItem>
+          </List>
+        </div>
+      </>
+    );
+  }
+
+  return (
+    <>
+      <Steps
+        steps={steps}
+        index={currentStepIndex}
+        onStepChange={setCurrentStepIndex}
+      />
+
+      <div
+        className={"充值额度"}
+        style={currentStep.value !== "setPrice" ? { display: "none" } : {}}
+      >
+        <div className="modal-mask">
+          <Modal title={"充值Key"} onClose={props.onClose}>
+            <List>
+              <ListItem title={"选择金额"} subTitle={"200p=1元"}>
+                <IconButton
+                  icon={<RmbIcon />}
+                  text={"5元"}
+                  className={styles["sidebar-bar-button"]}
+                  shadow
+                  onClick={() => {
+                    setPrice("5");
+                  }}
+                />
+
+                <IconButton
+                  icon={<RmbIcon />}
+                  text={"10元"}
+                  className={styles["sidebar-bar-button"]}
+                  shadow
+                  onClick={() => {
+                    setPrice("10");
+                  }}
+                />
+
+                <IconButton
+                  icon={<RmbIcon />}
+                  text={"20"}
+                  className={styles["sidebar-bar-button"]}
+                  shadow
+                  onClick={() => {
+                    setPrice("20");
+                  }}
+                />
+
+                <IconButton
+                  icon={<RmbIcon />}
+                  text={"50"}
+                  className={styles["sidebar-bar-button"]}
+                  shadow
+                  onClick={() => {
+                    setPrice("50");
+                  }}
+                />
+
+                <IconButton
+                  icon={<RmbIcon />}
+                  text={"100"}
+                  className={styles["sidebar-bar-button"]}
+                  shadow
+                  onClick={() => {
+                    setPrice("100");
+                  }}
+                />
+
+                <IconButton
+                  icon={<RmbIcon />}
+                  text={"500"}
+                  className={styles["sidebar-bar-button"]}
+                  shadow
+                  onClick={() => {
+                    setPrice("500");
+                  }}
+                />
+              </ListItem>
+
+              <ListItem title="自定义金额">
+                <Input type="text" value={price}></Input>
+              </ListItem>
+
+              <ListItem title="请选择支付方式">
+                <IconButton
+                  icon={<WeChatPay />}
+                  text={"微信支付"}
+                  onClick={() => {
+                    ShowQr(), setPayType("native"), showPayQr("native");
+                  }}
+                />
+                <IconButton
+                  icon={<AliPay />}
+                  text={"支付宝支付"}
+                  onClick={() => {
+                    setPayType("alipay"), showPayQr("alipay");
+                  }}
+                />
+              </ListItem>
+            </List>
+          </Modal>
+        </div>
+
+        <div>
+          {currentStep.value === "setQr"}
+          <Modal title={"付款信息"} onClose={props.onClose}>
+            <div className={"recharge"}>
+              <List>
+                <ListItem title="充值金额">
+                  <Input
+                    type="text"
+                    value={"充值金额：" + price}
+                    readOnly={true}
+                  ></Input>
+                </ListItem>
+
+                <ListItem title="订单号">
+                  <Input
+                    type="text"
+                    value={"订单号：" + orderID}
+                    readOnly={true}
+                  ></Input>
+                </ListItem>
+
+                <ListItem title="二维码">
+                  <button title="关闭"></button>
+                </ListItem>
+              </List>
+            </div>
+          </Modal>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export function SetRecharge(props: { onClose: () => void }) {
+  return (
+    <div className="modal-mask">
+      <Modal title={"余额充值"} onClose={props.onClose}>
+        <div style={{ minHeight: "40vh" }}>
+          <Recharge1 />
+        </div>
+      </Modal>
+    </div>
+  );
+}
+
+export function Recharge1() {
+  const steps = [
+    {
+      name: "充值金额",
+      value: "setPrice",
+    },
+    {
+      name: "支付信息",
+      value: "showQr",
+    },
+  ];
+  const { currentStep, setCurrentStepIndex, currentStepIndex } =
+    useSteps(steps);
+  const formats = ["text", "image"] as const;
+  type ExportFormat = (typeof formats)[number];
+
+  const [exportConfig, setExportConfig] = useState({
+    format: "image" as ExportFormat,
+    includeContext: true,
+  });
+
+  function updateExportConfig(updater: (config: typeof exportConfig) => void) {
+    const config = { ...exportConfig };
+    updater(config);
+    setExportConfig(config);
+  }
+
   const accessStore = useAccessStore();
 
   const [price, setPrice] = useState("");
@@ -179,8 +497,16 @@ export function SetRecharge(props: { onClose: () => void }) {
   }
 
   return (
-    <div className="modal-mask">
-      <Modal title={"充值Key"} onClose={props.onClose}>
+    <>
+      <Steps
+        steps={steps}
+        index={currentStepIndex}
+        onStepChange={setCurrentStepIndex}
+      />
+      <div
+        className={styles["message-exporter-body"]}
+        style={currentStep.value !== "setPrice" ? { display: "none" } : {}}
+      >
         <List>
           <ListItem title={"选择金额"} subTitle={"200p=1元"}>
             <IconButton
@@ -265,7 +591,32 @@ export function SetRecharge(props: { onClose: () => void }) {
             />
           </ListItem>
         </List>
-      </Modal>
-    </div>
+      </div>
+      {currentStep.value === "showQr" && (
+        <div className={styles["message-exporter-body"]}>
+          <List>
+            <ListItem title="充值金额">
+              <Input
+                type="text"
+                value={"充值金额：" + price}
+                readOnly={true}
+              ></Input>
+            </ListItem>
+
+            <ListItem title="订单号">
+              <Input
+                type="text"
+                value={"订单号：" + orderID}
+                readOnly={true}
+              ></Input>
+            </ListItem>
+
+            <ListItem title="二维码">
+              <button title="关闭"></button>
+            </ListItem>
+          </List>
+        </div>
+      )}
+    </>
   );
 }
